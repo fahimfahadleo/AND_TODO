4552277cd6eb38390c88a962ca4997534fce9a16
HridoyHasan <hridoyhasan14@gmail.com>
2019-11-23 16:24:13 +0600

    final

--- app/src/main/java/com/blueitltd/andtodo/ListviewActivity.java
+++ app/src/main/java/com/blueitltd/andtodo/ListviewActivity.java
@@ -1,7 +1,11 @@
 package com.blueitltd.andtodo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,19 +14,30 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListviewActivity extends AppCompatActivity {
     ImageButton addbutton;
     ListView listView;
 
     TextInputEditText todo,time;
+    MaterialButton add;
+    ArrayList<itemClass> users;
+    ListveiwAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,79 +48,88 @@ public class ListviewActivity extends AppCompatActivity {
         addbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onButtonShowPopupWindowClick(addbutton);
+                showDialoguebox();
             }
         });
 
 
+       users =new ArrayList<>();
+       adapter=new ListveiwAdapter(this,users);
+       listView.setAdapter(adapter);
 
     }
 
-    public void onButtonShowPopupWindowClick(View view) {
+    void showDialoguebox(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(ListviewActivity.this);
+        View DialogueView = getLayoutInflater().inflate(R.layout.popupwindow,null);
 
-        // inflate the layout of the popup window
-        final LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popupwindow, null);
+        todo=DialogueView.findViewById(R.id.todo);
+        time=DialogueView.findViewById(R.id.time);
+        add=DialogueView.findViewById(R.id.add);
 
-        // create the popup window
+        builder.setView(DialogueView);
+        final AlertDialog alertDialog=builder.create();
+        alertDialog.setCanceledOnTouchOutside(true);
 
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        final boolean focusable = true; // lets taps outside the popup also dismiss it
-
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-
-
-        int width = size.x;
-        final PopupWindow popupWindow = new PopupWindow(popupView, width-120, height, focusable);
-
-        // show the popup window
-        // which view you pass in doesn't matter, it is only used for the window tolken
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-        // dismiss the popup window when touched
-          todo  =popupView.findViewById(R.id.todo);
-          time=popupView.findViewById(R.id.time);
-        MaterialButton add=popupView.findViewById(R.id.add);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-               String todotext=String.valueOf(todo.getText());
-               String timetext=String.valueOf(time.getText());
-               View focusview=null;
-               boolean isok=true;
-               if(TextUtils.isEmpty(todotext)){
-                   todo.setError("Field Can Not Be Empty!");
-                   isok=false;
-                   focusview=todo;
-               }else if(TextUtils.isEmpty(timetext)){
-                   time.setError("Field Can Not Be Empty!");
-                   isok=false;
-                   focusview=time;
-               }
-
-               if(isok){
-                   popupWindow.dismiss();
-               }else{
+            public void onClick(View v) {
+                String todotext=String.valueOf(todo.getText());
+                String timetext=String.valueOf(time.getText());
+                View focusview=null;
+                boolean isok=true;
+                if(TextUtils.isEmpty(todotext)){
+                    todo.setError("Field Can Not Be Empty!");
+                    focusview=todo;
+                    isok=false;
+                }else if(TextUtils.isEmpty(timetext)){
+                    time.setError("Field Can Not Be Empty!");
+                    focusview=time;
+                    isok=false;
+                }
+                if(isok){
+                    users.add(new itemClass(todotext,timetext));
+                    adapter.notifyDataSetChanged();
+                    alertDialog.dismiss();
+                }else {
                     focusview.requestFocus();
                     isok=true;
-               }
 
+                }
             }
         });
-
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return true;
-            }
-        });
-
+        alertDialog.show();
+    }
+    class itemClass{
+        String work;
+        String time;
+        public itemClass(String todo,String time){
+            work=todo;
+            this.time=time;
+        }
     }
 
+    class ListveiwAdapter extends ArrayAdapter<itemClass>{
 
+        public ListveiwAdapter(@NonNull Context context,@NonNull ArrayList<itemClass> objects) {
+            super(context, 0, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if(convertView==null){
+                itemClass itemClass=getItem(position);
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.customlistviewlayout, parent, false);
+                TextView todotext=convertView.findViewById(R.id.todo);
+                TextView timetext=convertView.findViewById(R.id.time);
+                todotext.setText(itemClass.work);
+                timetext.setText(itemClass.time);
+            }
+
+
+            return convertView;
+        }
+    }
 }
